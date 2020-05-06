0f9292549a8b0c0ac17700ad4174a8b591670967
Victor Kmita <me@vkmita.com>
2020-05-06 15:10:05 +0700

    run prettier

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -1,4 +1,3 @@
-
 /** A class representing a linked list */
 export default class LinkedList {
   head: Node;
@@ -8,10 +7,10 @@ export default class LinkedList {
 
     let previousNode = new Node({ value: values.shift() });
     this.head = previousNode;
-    
-    while(values.length > 0) {
+
+    while (values.length > 0) {
       const node = new Node({ value: values.shift() });
-      previousNode.nextNode = node
+      previousNode.nextNode = node;
       previousNode = node;
     }
   }
@@ -19,12 +18,12 @@ export default class LinkedList {
   toValueArray = (): Array<any> => {
     let node = this.head;
     const array = [];
-    while(node) {
+    while (node) {
       array.push(node.value);
       node = node.nextNode;
     }
     return array;
-  }
+  };
 
   /**
    * head -> node1 -> node2 -> node3 ->
@@ -43,15 +42,15 @@ export default class LinkedList {
     let node2 = node1.nextNode;
     node1.nextNode = head;
 
-    while(node2) {
+    while (node2) {
       const node3 = node2.nextNode;
       node2.nextNode = node1;
 
       if (!node3) {
         this.head = node2;
-        return
+        return;
       }
-  
+
       node1 = node3;
       const tempNode2 = node3.nextNode;
       node1.nextNode = node2;
@@ -59,7 +58,7 @@ export default class LinkedList {
     }
 
     this.head = node1;
-  }
+  };
 }
 
 /** A class representing a node of a linked list */
@@ -67,7 +66,7 @@ export class Node {
   value: any;
   nextNode?: Node;
 
-  constructor({ value, nextNode }: { value: any, nextNode?: Node }) {
+  constructor({ value, nextNode }: { value: any; nextNode?: Node }) {
     this.value = value;
     this.nextNode = nextNode;
   }
--- src/__test__/LinkedList.test.ts
+++ src/__test__/LinkedList.test.ts
@@ -1,25 +1,25 @@
-import LinkedList from '../LinkedList';
+import LinkedList from "../LinkedList";
 
-describe('LinkedList', () => {
-  describe('constructor', () => {
-    it('creates a linked list from an array of values', () => {
+describe("LinkedList", () => {
+  describe("constructor", () => {
+    it("creates a linked list from an array of values", () => {
       const linkedList = new LinkedList([1, 2, 3]);
-      const { head } = linkedList; 
+      const { head } = linkedList;
       expect(head.value).toEqual(1);
       expect(head.nextNode.value).toEqual(2);
       expect(head.nextNode.nextNode.value).toEqual(3);
     });
   });
 
-  describe('toValueArray', () => {
-    it('turns the linked list into an array', () => {
+  describe("toValueArray", () => {
+    it("turns the linked list into an array", () => {
       const linkedList = new LinkedList([3, 3, 8]);
       expect(linkedList.toValueArray()).toEqual([3, 3, 8]);
-    })
+    });
   });
 
-  describe('reverse', () => {
-    it('reverses the linked list', () => {
+  describe("reverse", () => {
+    it("reverses the linked list", () => {
       let linkedList = new LinkedList([]);
       linkedList.reverse();
       expect(linkedList.toValueArray()).toEqual([]);
@@ -40,5 +40,5 @@ describe('LinkedList', () => {
       linkedList.reverse();
       expect(linkedList.toValueArray()).toEqual([6, 5, 4, 3, 2, 1]);
     });
-  })
-})
+  });
+});
